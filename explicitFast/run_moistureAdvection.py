0b9b8db77979372ba8a03b5656cc95921a973b42
statisdisc <will.a.m@hotmail.co.uk>
2020-03-05 10:29:37 +0000

    Modified test case parameters and updated solidBodyRotation velocity field to include smooth velocity field near boundaries

---
+++ explicitFast/run_moistureAdvection.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import numpy as np
+
+os.system( "./Allrun" )
+
+folder_xyz = os.path.join(sys.path[0], "moistureAdvection")
+os.system( "rm -rf {}".format(folder_xyz) )
+os.makedirs( folder_xyz )
+
+#times = range(0,1820,20)
+times = range(0,11,1)
+
+for time in times:
+    folder_time_xyz = os.path.join(folder_xyz, str(time))
+    if not os.path.exists( folder_time_xyz ):
+        os.makedirs( folder_time_xyz )
+    
+    folder_time = os.path.join(sys.path[0], str(time))
+    os.system( "cp {} {}/".format( os.path.join(folder_time,"*.xyz"), folder_time_xyz ) )
+        
+os.system( "cp -r {}/ $DROPBOX/PhD/2020/".format(folder_xyz) )
